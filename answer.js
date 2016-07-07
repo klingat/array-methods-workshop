c5cb1caa7cc4be6177a084060bb670cd343fa84f
Katarina <klingat@me.com>
2016-07-07 19:21:49 +0000

    added exercise 3, one-liner function

--- answer.js
+++ answer.js
@@ -44,3 +44,20 @@ function getPositives2 (array) {
 }
 
 getPositives2([1, -2, 3, -4, 5, -6, 7, 8]);
+
+
+// Exercise 3
+
+/* Write a function called filterArray that takes an array AND a function as arguments. 
+Your filter function should return a new array that contains only the elements where the 
+passed function returns a truthy value.
+
+NOTE: You are allowed to use Array.prototype.filter to answer this question.
+
+NOTE 2: This is a bit of a trick question, the answer is a one-liner :) */
+
+function filterArray (array, func) { return array.filter(func) }
+
+
+
+
